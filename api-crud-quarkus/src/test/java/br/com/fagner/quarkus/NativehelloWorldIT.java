c7c54b3505c0d22acfe2babfbf08d5c8d40e042c
Fagner Oliveira <fagner.soliver@gmail.com>
2021-04-23 05:14:59 -0300

    Primeiro commit

---
+++ api-crud-quarkus/src/test/java/br/com/fagner/quarkus/NativehelloWorldIT.java
@@ -0,0 +1,9 @@
+package br.com.fagner.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativehelloWorldIT extends helloWorldTest {
+
+    // Execute the same tests but in native mode.
+}
