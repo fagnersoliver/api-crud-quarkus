d1e95cd2fa9be720bf1b261f088a6c1c3efee4c3
Fagner Oliveira <fagner.soliver@gmail.com>
2021-04-25 10:04:18 -0300

    Atualizando as exemplo quarkus

--- api-crud-quarkus/src/test/java/br/com/fagner/quarkus/NativehelloWorldIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.fagner.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativehelloWorldIT extends helloWorldTest {
-
-    // Execute the same tests but in native mode.
-}
